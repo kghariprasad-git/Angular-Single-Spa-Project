5abaf62a5869e14dba4b098940416e1581ea6bb1
HariprasadK G <1022923@questgedctvm.com>
2020-04-03 14:01:19 +0530

    Converting FirstAngular app  to single page

--- src/app/core/app.routing.module.ts
+++ src/app/core/app.routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from '../home/home.component';
 import {LoginComponent} from '../login/login.component';
 import {MobileComponent} from '../mobile/mobile.component';
 import {FruitComponent} from '../fruit/fruit.component';
+import { APP_BASE_HREF } from '@angular/common';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'mobile', component: MobileComponent },
   { path: 'fruit', component: FruitComponent },
   {path : '', component : LoginComponent}
+  
 ];
 @NgModule({
   imports: [
@@ -17,6 +19,9 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
   ],
   declarations: []
 })
